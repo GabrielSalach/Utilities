7ba0b2f7c6033b730cee7df7cf4ac4baef5c359a
GabrielSalach <gabsalach@orange.fr>
2021-04-17 00:27:39 +0200

    file reading part 1

--- Percemaille/Percemaille.h
+++ Percemaille/Percemaille.h
@@ -7,41 +7,81 @@
 /* ---------- STRUCTS ---------- */
 
 typedef struct{
-    char**** matrix;
+    char (***matrix)[32];
+    int section_count, value_count;
     char path[64];
 }Pe_File;
 
 
 /* ---------- FUNCTIONS HEADERS ---------- */
 
-Pe_File             *pe_file_create(char *path);                                                    /* Creates a Pe_File struct */
-
-int                pe_read_file(char* path);                                                       /* Reads the content of the file */
+Pe_File*            pe_file_create(char *path);                                                 /* Creates a Pe_File struct */
+int                 pe_read_file(Pe_File* file);                                                /* Reads the content of the file */
+char*               pe_matrix_read(Pe_File* file, int i, int j, int k);                         /* Returns the string in position [i][j][k] in the matrix */
+void                pe_file_destroy(Pe_File* file);                                             /* Frees the memory of a Pe_File. */
 
 
 /* ---------- STRUCT ALLOCATION ---------- */
 
-Pe_File *pe_file_create(char *path) {
+Pe_File* pe_file_create(char* path) {
     Pe_File* rtrn_file;
+    rtrn_file = (Pe_File*) malloc(sizeof (Pe_File));
     strcpy(rtrn_file->path, path);
+    return rtrn_file;
 }
 
 /* ---------- FILE INPUT ---------- */
+/* PENSER A CHECK LE NOMBRE DE SECTIONS CAR SI SECTION = 0 IL FAUT LA REMPLACER PAR DEFAULT DANS LA MATRICE */
+int pe_read_file(Pe_File* file) {
+    FILE* stream;
+    int buffer; /* stored as int so i can know when i reach eof */
 
-int pe_read_file(char* path) {
-    FILE* file;
-    char rtrn_string[128];
-    char buffer[1];
+    int section_count, value_count;
+    section_count = 0;
+    value_count = 1;
 
-    file = fopen(path, "r");
+    stream = fopen(file->path, "r");
     if(file == NULL) {
         printf("The file does not exist.");
         return -1;
     }
-    fscanf(file, "%c", buffer);
-    printf("%s;\n", buffer);
-    printf("%d\n", memcmp("a", buffer, 2));
+
+    /* Loops through file to count the ammount of sections and values */
+    while((buffer = fgetc(stream)) != EOF) {
+        if(buffer == ';') {
+            while(buffer != '\n' && buffer != EOF) {
+                buffer = fgetc(stream);
+            }
+        } else if(buffer == '[') {
+            section_count++;
+            while(buffer != '\n' && buffer != EOF) {
+                buffer = fgetc(stream);
+            }
+        } else {
+            value_count++;
+            while(buffer != '\n' && buffer != EOF) {
+                buffer = fgetc(stream);
+            }
+        }
+    }
+    file->section_count = section_count;
+    file->value_count = value_count;
+    printf("Found %d Section and %d value(s)\n", section_count, value_count);
+    file->matrix = (char (***)[32]) malloc(sizeof(char*) * section_count * value_count * 2);
+
+    fseek(stream, 0, SEEK_SET);
+    /* TODO :
+        - Get the right values into the right place ffs
+    */
+
     
-    return 0;
+}
+    
+char* pe_matrix_read(Pe_File* file, int i, int j, int k) {
+    return file->matrix + (i * file->section_count + j * file->value_count + k);
+}
+
+void pe_file_destroy(Pe_File* file) {
+    free(file->matrix);
 }
 
--- Percemaille/main.c
+++ Percemaille/main.c
@@ -1,3 +1,35 @@
+#include "Percemaille.h"
+#include <string.h>
+#include <stdio.h>
+
 int main() {
+    /* char* matrix[2][2][2];
+    int i, j;
+    matrix[0][0][0] = "Cat1";
+    matrix[0][1][0] = "Val1";
+    matrix[0][1][1] = "valeur";
+    matrix[1][0][0] = "cat2";
+    matrix[1][1][0] = "val2";
+    matrix[1][1][1] = "valeur";
+    for(i = 0; i < 2; i++) {
+        if(strcmp(matrix[i][0][0], "Cat1") == 0) {
+            printf("found Cat 1 at %d\n", i);
+            for(j = 0; j < 2; j++) {
+                if(strcmp(matrix[i][j][0], "Val1") == 0) {
+                    printf("found val1 at %d %d\n", i, j);
+                    printf("[Cat1] val1 = %s - [%d][%d][%d]\n", matrix[i][j][j], i, j, j);
+                }
+            }
+        }
+        
+    } */
+
+    Pe_File* file;
+    file = pe_file_create("test.txt");
+    if(pe_read_file(file) == 0){
+        printf("0\n");
+    }
+    pe_file_destroy(file);
+    free(file);
     return 0;
 }
