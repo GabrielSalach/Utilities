06f8ed1238fc5409a89fd155e8d3063c2bfce8b3
GabrielSalach <gabsalach@orange.fr>
2021-04-17 14:50:34 +0200

    ça marche aps

--- Percemaille/Percemaille.h
+++ Percemaille/Percemaille.h
@@ -35,7 +35,7 @@ Pe_File* pe_file_create(char* path) {
 int pe_read_file(Pe_File* file) {
     FILE* stream;
     int buffer; /* stored as int so i can know when i reach eof */
-
+    unsigned int i, j, k;
     int section_count, value_count;
     section_count = 0;
     value_count = 1;
@@ -68,11 +68,69 @@ int pe_read_file(Pe_File* file) {
     file->value_count = value_count;
     printf("Found %d Section and %d value(s)\n", section_count, value_count);
     file->matrix = (char (***)[32]) malloc(sizeof(char*) * section_count * value_count * 2);
+    for(i = 0; i < section_count; i++) {
+        for(j = 0; j < value_count; j++) {
+            for(k = 0; k < 32; k++) {
+                pe_matrix_read(file, i, j, 0)[k] = 0;
+            }
+            for(k = 0; k < 32; k++) {
+                pe_matrix_read(file, i, j, 1)[k] = 0;
+            }
+        }
+    }
 
+    /* Reset des valeurs pour placer les variables aux bons emplacements */
+    section_count = 0;
+    value_count = 1;
+
+    
     fseek(stream, 0, SEEK_SET);
-    /* TODO :
-        - Get the right values into the right place ffs
-    */
+    while((buffer = fgetc(stream)) != EOF) {
+        if(buffer == ';') {
+            while(buffer != '\n' && buffer != EOF) {
+                buffer = fgetc(stream);
+            }
+        } else if(buffer == '[') {
+            i = 0;
+            value_count = 1;
+            buffer = fgetc(stream);
+            while(buffer != ']' && buffer != EOF && i < 32) {
+                pe_matrix_read(file, section_count, 0, 0)[i] = buffer;
+                buffer = fgetc(stream);
+                i++;
+            }
+            buffer = fgetc(stream);
+            section_count++;
+        } else {
+            i = 0;
+            while(buffer != '=' && buffer != EOF && i < 32) {
+                if(section_count == 0) {
+                    pe_matrix_read(file, section_count, value_count, 0)[i] = buffer;
+                    buffer = fgetc(stream);
+                } else {
+                    pe_matrix_read(file, section_count - 1, value_count, 0)[i] = buffer;
+                    buffer = fgetc(stream);
+                }
+                i++;
+            }
+            i = 0;
+            buffer = fgetc(stream);
+            while(buffer != '\n' && buffer != EOF && i < 32) {
+                if(section_count == 0) {
+                    pe_matrix_read(file, section_count, value_count, 1)[i] = buffer;
+                    buffer = fgetc(stream);
+                } else {
+                    pe_matrix_read(file, section_count - 1, value_count, 1)[i] = buffer;
+                    buffer = fgetc(stream);
+                }
+                i++;
+            }
+            value_count++;
+        }
+    }
+    printf("[%s] : %s=%s\n", pe_matrix_read(file, 0, 0, 0), pe_matrix_read(file, 0, 1, 0), pe_matrix_read(file, 0, 1, 1));
+    printf("[%s] : %s=%s\n", pe_matrix_read(file, 0, 0, 0), pe_matrix_read(file, 0, 2, 0), pe_matrix_read(file, 0, 2, 1));
+    return 0;
 
     
 }
@@ -82,6 +140,11 @@ char* pe_matrix_read(Pe_File* file, int i, int j, int k) {
 }
 
 void pe_file_destroy(Pe_File* file) {
-    free(file->matrix);
+    unsigned int i, j, k;
+    for(i = 0; i < file->section_count; i++) {
+        for(j = 0; j < file->value_count; j++) {
+            free(file->matrix + (i*file->section_count + j));
+        }
+    }
 }
 
